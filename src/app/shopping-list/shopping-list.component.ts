10befdbe345e15723a7d3b9dd05e79ba0fabe1ee
Sabina <sabina.vojic@habmann.info>
2023-10-16 09:37:23 +0200

    Prvi dio projekta - Add, Delete & Clear Buttons

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit{
+  ingredients: Ingredient[] = [
+    new Ingredient ('Apples', 5),
+    new Ingredient ('Bananas', 5),
+
+
+  ];
+
+  constructor(){
+
+  }
+
+  ngOnInit(){
+
+  }
+
+}
